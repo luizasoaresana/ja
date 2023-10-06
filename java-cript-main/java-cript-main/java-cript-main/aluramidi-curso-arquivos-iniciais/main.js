f909126cf3a0b45d45452967ed21bf1652638adf
luizasoaresana <108340502+luizasoaresana@users.noreply.github.com>
2023-10-06 14:14:07 -0300

    Add files via upload

---
+++ java-cript-main/java-cript-main/java-cript-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,20 @@
+// Pom-----------------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+   
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    
+    listaDeTeclas[contador].onclick = tocaSomPom; 
+
+contador = contador + 1;
+
+console.log(contador);
+
+} 
